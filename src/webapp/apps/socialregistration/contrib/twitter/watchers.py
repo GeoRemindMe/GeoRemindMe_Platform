8a36b586bc183270b7e866c28d0352ebc7265336
Javier Cordero <jneight@gmail.com>
2012-03-13 13:34:13 +0100

    reverse

---
+++ src/webapp/apps/socialregistration/contrib/twitter/watchers.py
@@ -0,0 +1,24 @@
+#coding=utf-8
+
+import logging
+logger = logging.getLogger(__name__)
+
+from django.dispatch import receiver
+
+from funcs import DEBUG
+from events.signals import suggestion_new
+
+
+@receiver(suggestion_new)
+def new_suggestion(sender,  instance, created, to_facebook=True, to_twitter=True, **kwargs):
+    if to_twitter and sender._is_public():
+        msg = "%(name)s %(url)s #grm" % {
+                                        'name': sender.name[:100] + '...',
+                                        'url': sender.short_url
+                                        }
+        try:
+            twitterprofile = sender.user.twitterprofile
+            twitterprofile.send_tweet(msg, sender.poi.location)
+            DEBUG("TWITTER: Enviado tweet sugerencia: %s" % sender)
+        except:
+            return
